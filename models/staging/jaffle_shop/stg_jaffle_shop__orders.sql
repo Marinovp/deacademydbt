e6fdd1f05fd2b6b8bc167bdcacb1d631cce3dbda
MarinoVP <Marinovp@users.noreply.github.com>
2026-05-16 17:37:30 -0400

    Development (#21)
    * lastest changes
    
    * organizing branch
    
    * Change materialization strategy
    
    * stripe payment table
    
    * adding freshness config
    
    * adding new source for stripe
    
    * generating models
    
    * Generic Tests: unique & not null
    
    * documentation
    
    * organizing code

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -9,24 +9,10 @@ source as (
 renamed as (
 
     select
-<<<<<<< HEAD
         id as order_id,
         user_id,
         order_date,
         status as order_status,
-=======
-<<<<<<< HEAD
-        id as order_id,
-        user_id,
-        order_date,
-        status as order_status,
-=======
-        id,
-        user_id,
-        order_date,
-        status,
->>>>>>> a7d72956406b90402a9d08aadaa7b1c20c2abde6
->>>>>>> b7d65f4a7df6a018675d1b502ae252c09d0f5bd9
         _etl_loaded_at
 
     from source
